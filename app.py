187fb0cee62699e33e1bbe964b793845350feaa6
sadilchamishka <sadilchamishka.16@cse.mrt.ac.lk>
2020-10-22 14:12:42 +0530

    convesation prediction route added

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask
 from UtterenceModel import predictUtterence
 from DeepLearntFeatures import featureMean,feature20BinMeans
+from ConversationModel import predictConversationOffline
 
 app = Flask(__name__)
 
-@app.route("/",methods = ['POST'])
-def UtterenceEmotionPrediction():
+@app.route("/")
+def home():
+        return "success"
+
+@app.route("/utterence",methods = ['POST'])
+def utterenceEmotionPrediction():
         file = request.files['audio']
         prediction = predictUtterence()
         print(prediction)
         return prediction
+
+@app.route("/conversation/offline",methods = ['POST'])
+def conversationEmotionPredictionOffline():
+        #file = request.files['audio']
+        prediction = predictConversationOffline("test")
+        return ""
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0')
